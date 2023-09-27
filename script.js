208e289682533bdbff61e58b1be17e69b78a54ed
CRedCodes <100335168+CRedCodes@users.noreply.github.com>
2023-09-27 17:22:43 -0400

    Adding a new set of notes, refer to the one before for oG

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Delay the hiding of the loading screen
+    console.log("DOMContentLoaded event fired.");
+    
     setTimeout(function () {
         document.querySelector(".loadingscreen").style.display = "none";
-    }, 4000); // 4 seconds (4000 milliseconds)
+    }, 4000); 
 });
-
 
