3e939834c0b7b5533e4837240867dbb795be7faa
CRedCodes <100335168+CRedCodes@users.noreply.github.com>
2023-09-26 22:14:11 -0400

    Attempt Screen Load

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Delay the hiding of the loading screen
+    setTimeout(function () {
+        document.querySelector(".loadingscreen").style.display = "none";
+    }, 4000); // 4 seconds (4000 milliseconds)
+});
+
+
