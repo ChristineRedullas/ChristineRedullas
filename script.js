1bd8130f87c09f4a29a9945fc01185688a32c481
CRedCodes <100335168+CRedCodes@users.noreply.github.com>
2023-09-30 02:46:51 -0400

    My new port

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("DOMContentLoaded event fired.");
-    
-    setTimeout(function () {
-        document.querySelector(".loadingscreen").style.display = "none";
-    }, 5000); 
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    let loadingScreen = document.getElementById('loadingScreen');
+
+    // Wait for 2 seconds and then fade out the loading screen
+    setTimeout(() => {
+        loadingScreen.style.transition = 'opacity 0.5s ease-out';
+        loadingScreen.style.opacity = '0';
+
+        // After the transition is done, remove the loading screen from the document
+        setTimeout(() => {
+            loadingScreen.parentNode.removeChild(loadingScreen);
+        }, 500);
+    }, 2000);
 });
 
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loadingScreen");
+    loader.style.display = "none";
+});
