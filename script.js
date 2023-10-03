dca0bc6730a53fd1b36858db02f5dc8cf89d7515
CRedCodes <100335168+CRedCodes@users.noreply.github.com>
2023-10-02 23:19:21 -0400

    borrowed code

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('.fullscreen-section');
-    const navLinks = document.querySelectorAll('#navbar .nav-link');
-
-    window.addEventListener('scroll', function () {
-        let current = '';
-        
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.clientHeight;
-            if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
-                current = section.getAttribute('id');
-            }
-        });
-
-        navLinks.forEach(link => {
-            link.classList.remove('active');
-            if (link.getAttribute('href').substring(1) === current) {
-                link.classList.add('active');
-            }
-        });
-    });
-});
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
